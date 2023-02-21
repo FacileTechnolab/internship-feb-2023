2f157792b0626a75c6842458a13bef1d4c698dc4
Krishika Maurya <krishika.facile@outlook.com>
2023-02-21 18:01:04 +0530

    C# Type casting, user input, operators, math class , object.

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -1,7 +1,73 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo
 {
-    private static void Main(string[] args)
+    internal class Program 
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            // string inp = Console.ReadLine();
+
+            // Console.WriteLine(inp);
+            // Console.WriteLine("Hello World");
+            // Console.WriteLine("Hello Krishika");
+            // Console.Write("i love C#");
+
+            //int a = 34;
+            //float b = 34.4F;
+            //double c = 34.4D;
+            //bool isGreat = true;
+            //char d = 'r';
+            //string e = "this is a string";
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //Console.WriteLine(c);
+            //Console.WriteLine(isGreat);
+            //Console.WriteLine(d);
+            //Console.WriteLine(e);
+
+            //int x = (int)3.5;
+            //double x1 = (double)3.5;
+            //Console.WriteLine(x1);
+
+            //int x = 3;
+            //double y = 4;
+            //float z = 'y';
+            //float varr = Convert.ToInt64(3.55);
+            //string sx = "44 is amazing";
+
+
+
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            //Console.WriteLine(z);
+
+            //Console.WriteLine("Enter your name");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Hey " + name + " its me! ");
+
+            //Console.WriteLine("How many candy do you want?");
+            //string can = Console.ReadLine();
+            //Console.WriteLine("you will get 4 more candies: "+ 
+            //    (Convert.ToInt32(can) + 4));
+
+            //int a = Math.Max(34, 345);
+            //double a = Math.Sqrt(36);
+            //Console.WriteLine(a);
+
+            var mk = new Person();
+            mk.Fname = "Krishika";
+            mk.lName = "Maurya";
+            mk.Introduce();
+
+
+
+
+            Console.ReadLine();
+        }
     }
 }
