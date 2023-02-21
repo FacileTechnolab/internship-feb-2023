167a3561b0ecf3643b2939ed23964d83690846aa
Dipali <dipali.facile@outlook.com>
2023-02-20 18:49:11 -0800

    datatypes typecasting

---
+++ week-2/c#demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("How are you?");  
+      Console.Write("Hello World...!");
+      Console.WriteLine("How is it fine?");  
+
+      Console.ReadLine();  
+
+    }
+  }
+}
