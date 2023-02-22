295f4d4e54378b7831ce044e1b2b70ab07db27bd
Krishika Maurya <krishika.facile@outlook.com>
2023-02-22 18:19:55 +0530

    C#  Arrays, String, Enum, Types, conditional statement.

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CsharpFundamentals.Math;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -59,11 +60,91 @@ namespace demo
             //double a = Math.Sqrt(36);
             //Console.WriteLine(a);
 
-            var mk = new Person();
-            mk.Fname = "Krishika";
-            mk.lName = "Maurya";
-            mk.Introduce();
+            //var mk = new Person();
+            //mk.Fname = "Krishika";
+            //mk.lName = "Maurya";
+            //mk.Introduce();
 
+            //Calculator calculator = new Calculator();
+            //var result = calculator.Add(1, 2);
+            //Console.WriteLine(result);
+            //var numbers = new int[3];
+            //numbers[0] = 1;
+
+            //Console.WriteLine(numbers[0]);
+            //Console.WriteLine(numbers[1]);
+            //Console.WriteLine(numbers[2]);
+
+            //var flags = new bool[3];
+            //flags[0] = true;
+
+            //Console.WriteLine(flags[0]);
+            //Console.WriteLine(flags[1]);
+            //Console.WriteLine(flags[2]);
+
+            //var names = new string[3] {"Krishika", "dipal", "sweta"};
+
+            //string[] days = { "Sunday", "Monday", "Tuesday", "wednesday" };//mynum={1,2,3,4,5};
+            //for (int i=0; i<days.Length; i++)
+            //Array.Sort(days);
+            //foreach (string i in days)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //int[,] number = { { 1, 4, 7 }, { 2, 8, 9 } };
+            //Console.WriteLine(number[1,2]);
+
+            //{
+            //    var firstName = "Krishika";
+            //    var lastName = "Maurya";
+
+            //    var fullName = firstName + "" + lastName;
+            //    var myfullName = string.Format("My name is {1}", firstName, lastName);
+            //    var name = new string[3] { "Krishika", "Jahanvi", "Dipali" };
+            //    var formattednames = string.Join(",", name);
+            //    Console.WriteLine(formattednames);
+            //}
+
+            //var a = 10;
+            //var b = a;
+            //b++;
+            //Console.WriteLine(string.Format("a: {0}, b: {1}", a,b));
+
+            //var array1 = new int[3] { 1, 2, 3};
+            //var array2 = array1;
+            //array2[0] = 0;
+            //Console.WriteLine(string.Format("array1[0]: {0}, array2[0]: {1}" , array1[0], array2[0] ));
+
+            /* condition statement*/
+            int hour = 10;
+            if( hour > 0 && hour < 12)
+            {
+                Console.WriteLine("its morning.");
+            }
+             else if (hour > 19 && hour < 18)
+            {
+                Console.WriteLine("its afternoon.");
+            }
+             else
+            {
+                Console.WriteLine("its evening.");
+            }
+
+            var season = Season.Autumn;
+            switch(season)
+            {
+                case Season.Autumn:
+                    Console.WriteLine("Its autumn and a beautiful season.");
+                    break;
+
+                case Season.Summer:
+                    Console.WriteLine("Its perfect to go beach.");
+                    break;
+
+                default:
+                    Console.WriteLine("I don't understand that season!");
+                    break;
+            }
 
 
 
@@ -71,3 +152,4 @@ namespace demo
         }
     }
 }
+ 
