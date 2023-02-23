52a87c534fcf2795f0f6451c3d17a98220e07803
shivangi <shivangi.facile@outlook.com>
2023-02-22 18:24:27 -0800

    add some code  for  list, Array, class&object enums

---
+++ c sharp/ArrayandList/ArrayandList/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace ArrayL
+{
+    class ArrayandList
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 1, 2, 3,4 };
+
+            Console.WriteLine("length : "+numbers.Length);
+            var index = Array.IndexOf(numbers, 2);
+
+            Console.WriteLine("index of 4 is :" + index);
+            Array.Clear(numbers, 0, 2);
+
+            Console.WriteLine("effect of Clear()");
+
+            foreach(var i in numbers)
+                Console.WriteLine(i);
+
+            int[] another = new int[3];
+
+            Array.Copy(numbers, another, 3);
+            Console.WriteLine("Effect of copy");
+            foreach (int h in another)
+                Console.WriteLine(h);
+
+            Array.Sort(numbers);
+            Console.WriteLine("effect of short");
+                foreach(int j in numbers)
+                    Console.WriteLine(j);
+
+                Array.Reverse(numbers);
+
+            Console.WriteLine("reverse of array");
+            foreach(int l in numbers) 
+                Console.WriteLine(l);
+            int y = 64;
+            Console.WriteLine(Math.Sqrt(y));
+        }
+    }
+}
---
+++ c sharp/Csharpfundamental/Csharpfundamental/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Fundamental
+{
+   public class Person
+    {
+        public int age;
+
+    }
+
+    class Csharpfundamental
+    {
+        static void Main(string[] args)
+        {/*
+            var a = 9;
+            var b = a;
+            b++;
+            Console.WriteLine(string.Format("a:{0} , b:{1}", a, b));
+            var array = new int[3] { 1, 2, 3 };
+            var array2 = array;
+            array2[0] = 3;
+            Console.WriteLine(string.Format("array[0]:{0}, array2[0]:{1},array[0],array2[0]"));
+            */
+            var number = 1;
+            Increments(number);
+           Console.WriteLine(number);
+            var person=new Person() { age=20};
+            Old(person);
+            Console.WriteLine(person.age);
+            
+            var random=new Random();
+            for(var i=0;i<10;i++)
+            
+                Console.WriteLine(random.Next(1,10));
+            
+        }
+        public static void Increments(int number) 
+        {
+            number += 10;
+
+        }
+        public static void Old(Person person)
+        {
+            person.age += 10;
+
+        }
+
+    }
+}
