667b81277c64bc6e6ad8cee7fbee511488171482
krishika <krishika.facile@outlook.com>
2023-03-06 18:19:27 -0800

    java script Video learn about  Operators .

--- java script/index.js
+++ java script/index.js
@@ -4,23 +4,122 @@
 // let firstName = undefined;
 // let selectedColor = null;
  
-let person = {
-    name :'Krishika',
-    age: 30
-};
+// let person = {
+//     name :'Krishika',
+//     age: 30
+// };
 
-//dot notation
-person.name='dipali';
+// //dot notation
+// person.name='dipali';
+// //Bracket Notation
+// person['name'] = 'sweta';
+// console.log(person.name);
 
-//Bracket Notation
-person['name'] = 'sweta';
-console.log(person.name);
+// let selectedColors=['red','blue'];
+// selectedColors[2]=1;
+// console.log(selectedColors.length);
 
-let selectedColors=['red','blue'];
-selectedColors[2]=1;
-console.log(selectedColors.length);
+// function greet(name, lastName){
+//     console.log('Hello '+ name+' ' + lastName);
+// }
+// greet('krishika','Maurya');
 
-function greet(name, lastName){
-    console.log('Hello '+ name+' ' + lastName);
-}
-greet('krishika','Maurya');
+
+ /*OPERATOR
+let x = 10;
+let y = 3;
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+console.log(x**y);
+console.log(x++);
+console.log(x);
+console.log(--x);
+
+console.log (x+=5);
+console.log (x-=5);
+console.log (x*=5);
+console.log (x/=2);
+
+let x=1;
+
+console.log(x>1);
+console.log(x<1);
+console.log(x>=1);
+console.log(x<=1);
+console.log(x===1);
+console.log(x!==5);
+
+console.log(1===1);
+console.log('1'===1);
+console.log(1==1);
+console.log('1'==1);
+
+let points=90;
+let type=points>100?'gold':'Silver';
+
+console.log(type);
+
+let highIncome=false;
+let goodCreditScore=false;
+let eligibleForLoan= highIncome || goodCreditScore;
+console.log('Eligible', eligibleForLoan);
+let applicationRefused=!eligibleForLoan;
+console.log('Application Refused',applicationRefused);
+
+let x=2+3*5;
+console.log(x);
+
+let a= 'red';
+let b='blue';
+
+let c=a;
+a=b;
+ b=c;
+
+console.log(a);
+console.log(b);
+
+let x = 10;
+let y = (x > 5) && (x < 15); 
+let x = 5; 
+console.log (x += 3); */
+
+/* control flow */
+  
+// let hour =20;
+// if (hour>=6 && hour<12)
+//     console.log('Good Morning');
+// else if (hour>=12 && hour<18)
+//     console.log('Good Afternoon');
+// else
+//     console.log('Good Evening');
+     
+let role ='';
+ switch(role)
+ {
+    case 'guest':
+        console.log('Guest User');
+        break;
+     case 'moderator':
+        console.log('Moderator User');
+        break;
+    
+    default:
+        console.log('Unknown User');
+ }
+
+
+
+
+
+
+
+
+
+
+
+
