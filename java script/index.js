30e5ec8bd593377e51c4ac98e9ffdc6991bfe34e
krishika <krishika.facile@outlook.com>
2023-03-07 18:22:21 -0800

    conditional statement, loops in control flow,  fectory function and constructor function in object

--- java script/index.js
+++ java script/index.js
@@ -98,26 +98,140 @@ console.log (x += 3); */
 // else
 //     console.log('Good Evening');
      
-let role ='';
- switch(role)
- {
-    case 'guest':
-        console.log('Guest User');
-        break;
-     case 'moderator':
-        console.log('Moderator User');
-        break;
+// let role ='';
+//  switch(role)
+//  {
+//     case 'guest':
+//         console.log('Guest User');
+//         break;
+//      case 'moderator':
+//         console.log('Moderator User');
+//         break;
     
-    default:
-        console.log('Unknown User');
- }
+//     default:
+//         console.log('Unknown User');
+//  }
 
 
+ /*LOOP
+for(let i=0; i<5; i++)
+{
+    console.log('Hello World');
+}
+let i=1;
+while (i<=6)
+{
+    if (i%3 !== 0) console.log(i);
+    i++;
+}
+let i=3;
+do{
+    if(i%2!==0) console.log(i);
+    i++;
+}while(i<=5);
+  
+let i=0;
+while(i<5){
+    console.log(i);
+    //i++;
+}
 
+while(true)
+{
+}
 
+let x=0;
+do{
+    x++;
+}while(x<5);
 
+for(let i=0; i<10;)
 
+const person={
+    name:'Krishika',
+    age:21};
+for(let key in person)
+console.log(key, person[key]) ;
 
+const colors=['red','green','blue'];
+for(let color of colors)
+console.log( color);
+
+ let i=0;
+ while(i<=10){
+    if(i===8)break;
+    if(i%2===0){
+        i++;
+        continue;
+    }
+    console.log(i);
+    i++;
+ }*/
+
+/*  maximum to number*/
+//  let number=max(3,3);
+//  console.log(number);
+
+//  function max(a,b){
+//     return (a>b) ? a : b;
+//  }
+//  /* Landscap and protrait */
+//  console.log(isLandscape(800,600));
+//   function isLandscape(width , height)
+//   {
+//     return (width>height);
+//   }
+  /*FizzBuzz*/
+//     const output= fizzBuzz(15);
+//     console.log(output);
+//     function fizzBuzz(input) { 
+
+//         if((input%3===0) && (input%5===0))
+//         return'FizzBuzz';
+
+//         if (typeof input!=='number')
+//         return'Not a number';
+
+//         if(input%3===0)
+//         return'Fizz';
+
+//         if(input%5===0)
+//         return'Buzz';
+
+//         return input;
+// }
+ /*Demerit point*/
+    // checkSpeed(130);
+    // function checkSpeed(speed)
+    // {
+    //     const speedLimit=70;
+    //     const kmPerPoint=5;
+
+    //     if(speed<speedLimit+kmPerPoint)
+    //     {
+    //         console.log('ok');
+    //         return;
+    //     }
+    //     const point=Math.floor((speed-speedLimit)/kmPerPoint)
+    //     if(point>=12)
+    //         console.log('License suspended');
+    //     else
+    //     console.log('Points',points);
+
+    // }
+/*Even and Odd*/    
+ 
+// showNumbers(10);
+
+// function showNumbers(Limit)
+// {
+//     for(let i=0; i<=Limit; i++)
+//     {
+//         const message=(i%2===0)?'EVEN':'ODD'
+//         console.log(i,message);
+//     }
+// }
+  
 
 
 
---
+++ java script/object.js
@@ -0,0 +1,23 @@
+// function createCircle(radius)
+// {
+//     return{
+//         radius,
+//         draw(){
+//             console.log('draw');
+//         }
+//     };
+// }
+//  const circle1=createCircle(1);
+//  console.log(circle1);
+
+//  const circle2=createCircle(2);
+//  console.log(circle2);
+  
+function Circle(radius) {
+        this.radius = radius;
+        this.draw = function () {
+            console.log('draw');
+        };
+    }
+ const circle=new Circle(1);
+ 
