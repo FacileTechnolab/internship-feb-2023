86a5bbe5c91fd7a0db33d9c769e41df7954487f4
krishika <krishika.facile@outlook.com>
2023-03-09 19:55:45 -0800

    Dynamic Nature of Objects, Constructor Property, Functions are Objects, date, Cloning an Object, Template Literals, String, Math, Garbage Collection, Enumerating Properties, Cloning an Object, Value vs. Reference Type in Objects.

--- java script/object.js
+++ java script/object.js
@@ -12,12 +12,68 @@
 
 //  const circle2=createCircle(2);
 //  console.log(circle2);
+
+// const myCircle=createCircle(1);
   
-function Circle(radius) {
-        this.radius = radius;
-        this.draw = function () {
-            console.log('draw');
-        };
+// function Circle(radius) {
+//         this.radius = radius;
+//         this.draw = function () {
+//             console.log('draw');
+//         };
+//     }
+//  const circle=new Circle(1);
+  
+// const circle={
+//     radius:1
+// };
+
+// circle.color='yellow';
+// circle.draw=function(){}
+
+// delete circle.color;
+// delete circle.draw;
+
+// console.log(circle);
+
+// const now = new Date();
+// const date1=new Date ('january 7 2002 02:00');
+// const date2=new Date(2021,9,8,5);
+// now.setFullYear(2022);
+
+// const name ='Krishika';
+// const message = 'Hi ' + name + ',\n';
+// const another =
+// `Hi Krishika,
+// Thank you for joining my mailing  List.
+// Regards,
+// Maurya`;
+
+// const message='This is my first message';
+// const another = new String('hi');
+
+const circle={
+    radius:1,
+    draw(){
+        console.log('draw');
     }
- const circle=new Circle(1);
+};
+// for(let key in circle)
+// console.log(key, circle[key]);
  
+// for(let key of Object.keys(circle))
+// console.log(key);
+
+// for(let entry of Object.entries(circle))
+// console.log(entry);
+
+// if ('color' in circle) console.log('yes');
+
+// const another = {};
+// for(let key in circle )
+// another[key] = circle[key];
+
+//const another= Object.assign({},circle);
+
+ const another={...circle};
+
+console.log(another); 
