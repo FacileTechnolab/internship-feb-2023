f5daef870be0df1ead186f49b90e20441d96d10e
krishika <krishika.facile@outlook.com>
2023-03-15 18:50:32 -0700

    WHERE clause, AND, OR, IN, BETWEEN, LIKE, REGEXP, IS NULL,  in Data From a Single Table  .

--- SQL/Basic Query.sql
+++ SQL/Basic Query.sql
@@ -1,4 +1,29 @@
 use sakila;
 
-select* from customer
-where customer_id =2 ;
+ 
+-- select  
+-- payment_id,
+-- customer_id,
+--  amount,
+-- amount * 10 + 100 AS pay
+-- from payment;
+
+-- select * from payment
+-- where not(amount < 10 and rental_id < 13000);
+
+-- select * from payment
+-- where payment_id between  '32' and '39'
+
+-- select *from customer
+-- where first_name  LIKE '__a__'
+
+-- select * from customer
+-- where last_name regexp '[thdjsbfl*]'
+
+select * from address
+where district IS NULL
+
+
+
+
+
