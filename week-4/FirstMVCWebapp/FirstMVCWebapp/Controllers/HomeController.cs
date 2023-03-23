9b10e09fbdb3605c1f128e5831dbc0487a786320
Dipali <dipali.facile@outlook.com>
2023-03-22 18:32:25 -0700

    Action Parameters

---
+++ week-4/FirstMVCWebapp/FirstMVCWebapp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using FirstMVCWebapp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FirstMVCWebapp.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Student student = new Student();
+            student.Name = "Dipali";
+            student.City = "Ahmedabad";
+            return View(student);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+       
+    }
+}
