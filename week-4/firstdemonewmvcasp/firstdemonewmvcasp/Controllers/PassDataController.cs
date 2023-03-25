d8dba3b9b5b59a079a373de47fa3ef6375746c02
Dipali <dipali.facile@outlook.com>
2023-03-24 18:28:41 -0700

    Partial Views, Passing Data to Views, Razor Syntax

---
+++ week-4/firstdemonewmvcasp/firstdemonewmvcasp/Controllers/PassDataController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace firstdemonewmvcasp.Controllers
+{
+    public class PassDataController : Controller
+    {
+        // GET: PassData
+        public ActionResult Index()
+        {
+            return View("PassData");
+        }
+    }
+}
