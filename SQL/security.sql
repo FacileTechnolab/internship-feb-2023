df7775a7abaae72e81deac46b79b127d78f784d4
krishika <krishika.facile@outlook.com>
2023-03-27 18:24:12 -0700

    Stored Procedures( what, create, Dropping, Variables, function)

--- SQL/security.sql
+++ SQL/security.sql
@@ -6,3 +6,12 @@ create user uv Identified by 'uv@123';
 --  drop user;
 create user pk@localhost identified by 'pk@123';
 drop user pk@localhost;
+
+-- grant 
+create user moon_app identified by '3656';
+grant select, insert, update, delete , execute
+on student.*	
+to moon_app;
+
+ show grants for moon_app;
+
