ef68b2db9bab2c66b485a841e693e98904d8afcb
jahanvi <jahanvi.facile@outlook.com>
2023-04-03 18:42:29 +0530

    Add FIle about Action Result,Parameter,Attribute Routing,Partial view

---
+++ week4/.net MVC/WebApplication1/Controllers/userController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class userController : Controller
+    {
+        // GET: user
+        public ActionResult Login1()
+        {
+            ViewBag.Id = 1;
+            ViewBag.name = "Jahanvi";
+
+            ViewData["Id"] = 2;
+            Student student = new Student();
+            student.ID = "1000";
+            student.Name = "joy";
+            ViewBag.std=student;
+
+            return View(student);
+        }
+    }
+}
