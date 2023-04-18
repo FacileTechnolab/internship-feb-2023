c3d56a1b04b41124a961441753f7180499f0123e
jahanvi <jahanvi.facile@outlook.com>
2023-04-18 14:50:14 +0530

    #add file about javascript and c#..

---
+++ week2/javascript/operator.js
@@ -0,0 +1,76 @@
+//arithmetic operator
+// var a =10;
+// var b= 3;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);//power of a 10*3
+// //increment 
+//  console.log(a++);//10
+//  console.log(++a);//11
+// //decrement
+// console.log(--a);//9
+// console.log(a--);//10
+//assignment operator
+// let x =10;
+// //x +=5;
+// //x -=5;
+// x *=5;
+// console.log(x);
+//comparision operator
+// let d=1;
+// console.log(d>0);
+// console.log(d>=1);
+// console.log(d<1);
+// console.log(d<=1);
+// //EQUALITY
+// console.log(d===1);
+// console.log(d!==1);
+//strict equality  (value+type)
+// console.log(1===1);//t
+// console.log('1'===1);//f
+// //loose equality(value)
+// console.log(1==1);//t
+// console.log('1'==1);//t
+// console.log(1===1);//t
+// console.log(true==1);//f
+
+// console.log(true==true);//t
+// //ternary operator
+// let points = 110;
+// let type = points>100?'gold' : 'silver';
+// console.log(type);
+//logical operator
+//and 
+// console.log(true && true);
+// let inverstLoan = true;
+// let bank = true;
+// let loan = inverstLoan && bank;
+// console.log(loan);
+//or
+
+let inverstLoan = true;
+let bank = true;
+let loan = inverstLoan || bank;
+console.log(loan);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
