ac5cd3d6d41cc1709a60ac7d1ea0be068c18d099
Dipali <dipali.facile@outlook.com>
2023-04-17 18:39:50 -0700

    authonticaion

--- week-4/AuthenticationAsp.netMvc/AuthenticationAsp.netMvc/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AuthenticationAsp.netMvc.MvcApplication" Language="C#" %>
---
+++ week-4/AuthonticationMvc/AuthonticationMvc/App_Start/RouteConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace AuthonticationMvc
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapMvcAttributeRoutes();
+
+            routes.MapRoute(
+                "ByRealeseDate",
+                 "ConventionbaseRoute/Realese/{year}/{month}",
+                 new { Controller = "ConventionbaseRoute", Action = "ByRealeseDate" },
+                 new { Year = @"\d{2}", Month = @"\d{2}"}
+                );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
