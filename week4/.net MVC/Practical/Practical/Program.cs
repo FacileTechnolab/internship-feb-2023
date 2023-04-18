847c43e25e7b5219239b024a11110ad1ec48989f
jahanvi <jahanvi.facile@outlook.com>
2023-04-18 18:14:36 +0530

    # Add files about Maxmin , Swapping , Oddeven.

---
+++ week4/.net MVC/Practical/Practical/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter number");
+            //int number = Convert.ToInt16(Console.ReadLine());
+
+
+            //if (number % 2 == 0)
+            //{
+            //    Console.WriteLine("even");
+            //    Console.ReadLine();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("odd");
+            //    Console.ReadLine();
+            //}  
+
+
+            //****MaxMinBase min
+            //int a = 10;
+            //int b = 20;
+            //int ret;
+            //MaxMinBase n = new MaxMinBase();
+
+            //ret = n.FindMax(a, b);
+            //Console.WriteLine("Max value is:{0}", ret);
+            //Console.ReadLine();
+
+
+            //*******Swaping
+            // int x = 10;
+            //int y = 20;
+            //Swaping s = new Swaping();
+
+            //s.Swap(x, y);
+            //Console.WriteLine();
+
+            //Console.ReadLine();
+
+
+            
+
+        }   
+    }
+}
+    
---
+++ week4/.net MVC/Practical/Practical/Swaping.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical
+{
+    internal class Swaping
+    {
+        public void Swap(int num1,int num2)
+        {
+           
+            Console.WriteLine("\nEnter number a= " + num1);
+            Console.WriteLine("Enter number b=" + num2);
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            num1 = num1 - num2;
+
+
+            Console.WriteLine("Enter number a= " + num1);
+            Console.WriteLine("Enter number b=" + num2);
+
+
+        }
+    }
+}
+
+    
+
