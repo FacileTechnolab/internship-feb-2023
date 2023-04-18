9974467d5a63cccbc7dd2831f186e75d2aa4fbd7
krishika <krishika.facile@outlook.com>
2023-04-18 12:14:22 -0700

    #clean-up week-4

--- SQL/index.sql
+++
@@ -1,5 +0,0 @@
-use student;
-explain select stu_id from stu_add where city ='nagpur';
-create index idx_city on stu_add(city);
-
-show indexes in stu_add;
--- SQL/tran-con.sql
+++
@@ -1,5 +0,0 @@
-use student;
-start transaction ;
-update stu_add set city = 'vadodra' where stu_id=1;
-update studentdetail  set marks = 50 where stu_id=4;
-commit;
