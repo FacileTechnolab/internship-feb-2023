370071dc6ec149a76507e11ccbb7f8f2e14b7cfa
jahanvi <jahanvi.facile@outlook.com>
2023-04-19 09:52:57 +0530

    #clean up

---
+++ week4/.net MVC/Practical/Practical/Reverse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical
+{
+    internal class Reverse
+    {
+        public int Rev()
+        {
+
+            int[] mynum = { 12, 16, 15, 13, 18 };
+            Array.Reverse(mynum);
+            foreach (int i in mynum)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+        }
+       
+
+}
+}
+    
+
---
+++ week4/.net MVC/Practical/Practical/ReverseBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Practical
+{
+    internal class ReverseBase
+    {
+        public int Sort()
+        {
+
+            int[] mynum = { 12, 16, 15, 13, 18 };
+            Array.Reverse(mynum);
+            foreach (int i in mynum)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+        }
+    }
+}
