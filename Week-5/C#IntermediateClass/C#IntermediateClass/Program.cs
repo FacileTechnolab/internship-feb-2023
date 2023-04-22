3053ccad64ad2498cf9e8989b3f243939b0ca572
shivangi <shivangi.facile@outlook.com>
2023-04-21 17:33:40 -0700

    Add create C# untermediate app in  create a class

---
+++ Week-5/C#IntermediateClass/C#IntermediateClass/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_IntermediateClass
+{
+    public class Person
+    {
+        public string Name;
+        public void Introduction(string to)
+        {
+            Console.WriteLine("hi {0}, I  am {1}", to, Name);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.Name = "shivangi";
+            person.Introduction("shah");
+            Console.ReadLine();
+        }
+    }  
+}
