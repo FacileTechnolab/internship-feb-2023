a8d2a00ffce5142253471d5687572f3185ec02d8
DESKTOP-F6DTSD6\admin <krishika.facile@outlook.com>
2023-04-24 13:32:28 -0700

    # Add Classes app in C# intermediate

---
+++ week-5/Classes/Classes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Person
+    {
+        public string name;
+        public void Introduce(string to)
+        {
+            Console.WriteLine("HI {0}, I am {1}", to , name);  
+        }
+         public static Person Parse(string str )
+         { 
+            var person = new Person();  
+            person.name = str;
+            return person;
+         }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person =Person.Parse("Krishika");
+            person.Introduce ("Maurya");
+            Console.ReadLine();
+        }
+    }
+}
