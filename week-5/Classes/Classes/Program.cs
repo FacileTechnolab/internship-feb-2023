1f1fa0527a394570215b80dc62ded968721c0063
DESKTOP-F6DTSD6\admin <krishika.facile@outlook.com>
2023-04-24 19:22:24 -0700

    # C# Constructors.

--- week-5/Classes/Classes/Program.cs
+++ week-5/Classes/Classes/Program.cs
@@ -6,28 +6,40 @@ using System.Threading.Tasks;
 
 namespace Classes
 {
-    public class Person
-    {
-        public string name;
-        public void Introduce(string to)
-        {
-            Console.WriteLine("HI {0}, I am {1}", to , name);  
-        }
-         public static Person Parse(string str )
-         { 
-            var person = new Person();  
-            person.name = str;
-            return person;
-         }
-    }
-
-    internal class Program
+    //public class Person
+    //{
+    //    public string name;
+    //    public void Introduce(string to)
+    //    {
+    //        Console.WriteLine("HI {0}, I am {1}", to , name);  
+    //    }
+    //     public static Person Parse(string str )
+    //     { 
+    //        var person = new Person();  
+    //        person.name = str;
+    //        return person;
+    //     }
+    //}
+    public class Program
     {
         static void Main(string[] args)
         {
-            var person =Person.Parse("Krishika");
-            person.Introduce ("Maurya");
+            //var person =Person.Parse("Krishika");
+            //person.Introduce ("Maurya");
+            //Console.ReadLine();
+
+            var customer = new Customer();
+            customer.ID = 1;
+            customer.Name = "Krishika";
+
+            var order = new Order();
+            //customer.Orders = new List<Order>();
+            customer.Orders.Add(order);
+
+            Console.WriteLine(customer.ID);
+            Console.WriteLine(customer.Name);  
             Console.ReadLine();
         }
+
     }
-}
+} 
