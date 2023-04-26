03bb29db2797c4c87af13a95ec5060baf6e7fc54
DESKTOP-F6DTSD6\admin <krishika.facile@outlook.com>
2023-04-26 15:44:18 -0700

    # c# Intermediate  add Classes.

--- week-5/Classes/Classes/Program.cs
+++ week-5/Classes/Classes/Program.cs
@@ -20,6 +20,7 @@ namespace Classes
     //        return person;
     //     }
     //}
+    
     public class Program
     {
         static void Main(string[] args)
@@ -28,17 +29,44 @@ namespace Classes
             //person.Introduce ("Maurya");
             //Console.ReadLine();
 
-            var customer = new Customer();
-            customer.ID = 1;
-            customer.Name = "Krishika";
+            //var customer = new Customer();
+            //customer.ID = 1;
+            //customer.Name = "Krishika";
 
-            var order = new Order();
-            //customer.Orders = new List<Order>();
-            customer.Orders.Add(order);
+            //var order = new Order();
+            ////customer.Orders = new List<Order>();
+            //customer.Orders.Add(order);
 
-            Console.WriteLine(customer.ID);
-            Console.WriteLine(customer.Name);  
-            Console.ReadLine();
+            //Console.WriteLine(customer.ID);
+            //Console.WriteLine(customer.Name);  
+
+            //var number = int.Parse("abc");
+
+        }
+        static void UserParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 3));
+            Console.WriteLine(calculator.Add(1, 2, 3, 4));
+            Console.WriteLine(calculator.Add(new int[] { 1, 2, 3, 4, 5 }));
+        }
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(new Point(101, 202));
+                Console.WriteLine("point is at ({0}, {1})", point.x, point.y);
+
+                point.Move(100, 200);
+                Console.WriteLine("point is at ({0}, {1})", point.x, point.y);
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An unexpected error occured.");
+            }
         }
 
     }
