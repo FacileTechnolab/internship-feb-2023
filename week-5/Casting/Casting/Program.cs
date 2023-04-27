96c40c93d57466a0a0589b779bc3e00f676ad9ab
jahanvi <jahanvi.facile@outlook.com>
2023-04-27 14:54:11 +0530

    #Add files about C# Intermediate.

---
+++ week-5/Casting/Casting/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casting
+{
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Text text = new Text();
+            //Shape shape = text;
+            //text.Width = 500;
+            //shape.Height = 200;
+            //Console.WriteLine(text.Width);
+            //Console.WriteLine(shape.Height);
+            //Console.ReadLine();
+            //***********UPCASTING****************
+            //StreamReader reader = new StreamReader(new MemoryStream());
+            //var list = new ArrayList();
+            //list.Add(1);
+            //list.Add("jahanvi");
+            //Console.ReadLine();
+            //list.Add(new Text());
+            //var anotherList=new List<Shape>();
+            //**************DOWNCASTING**********************
+            //Shape shape = new Text();
+
+
+            //************BOXING-UNBOXING*****************************
+            //var list = new ArrayList();
+            //list.Add(1);
+            //list.Add("jahanvi");
+            //list.Add(DateTime.Today);
+            //var anotherList=new List<int>();
+            //var names=new List<string>();
+           
+           //*********Method OVERRIDING*****************
+           var shapes=new List<Shape>();
+            shapes.Add(new Circle());
+            shapes.Add(new Rectangle());
+            var canvas=new canvas();
+            canvas.DrawShapes(shapes);
+
+
+        }
+    }
+}
---
+++ week-5/Casting/Casting/Shape.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casting
+{
+    public class CIrcle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a circle");
+        }
+
+    }
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a Rectangle");
+        }
+    }
+    public class Shape
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public Position position { get; set; }
+   
+        public virtual void Draw()
+        {
+
+        }
+    }
+}
