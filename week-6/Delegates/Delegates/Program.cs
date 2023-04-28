2a7b71dabeacb94f0aef815c5dbf1818588a0990
DESKTOP-F6DTSD6\admin <krishika.facile@outlook.com>
2023-04-28 16:56:39 -0700

    # Delegates and Lambda.

---
+++ week-6/Delegates/Delegates/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public  class Program
+    {
+        private static PhotoProcessor.PhotoFilterHandler RemovedRedEyeFilter;
+
+        static void Main(string[] args)
+        {
+            var processor = new PhotoProcessor();
+            var filter = new PhotoFilters();
+
+            Action<Photo> filterHandler = filter.ApplyBrightness;
+            filterHandler += filter.ApplyContrast;
+            filterHandler += RemoveRedEyeFilter;
+
+            processor.Process("photo.jpg",filterHandler);
+
+
+        }
+         static void RemoveRedEyeFilter(Photo photo)
+        {
+            Console.WriteLine("Apply RemoveRedEye");
+            Console.ReadLine();
+        }
+    }
+}
