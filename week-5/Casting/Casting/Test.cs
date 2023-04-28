9c98b7f176a1dacec505d9b5cb22fb4b35a2326b
DESKTOP-F6DTSD6\admin <krishika.facile@outlook.com>
2023-04-27 19:41:05 -0700

    #Properties,  Indexers, Class Coupling, Inheritance,Composition, Favor Composition over Inheritance, Access Modifiers, Constructors and Inheritance,  Upcasting and Down casting, Boxing and Unboxing

---
+++ week-5/Casting/Casting/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casting
+{
+   public class Test: Shape
+    {
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
+
+    }
+}
