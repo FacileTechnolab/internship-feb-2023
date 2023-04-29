1f96058c37d8b9d53fada198c61b718d34662aad
DESKTOP-F6DTSD6\admin <krishika.facile@outlook.com>
2023-04-28 19:12:59 -0700

    #Extension Method in c# Advance.

---
+++ week-6/ExtensionMethod/ExtensionMethod/A.cs
@@ -0,0 +1,17 @@
+﻿namespace ExtensionMethod
+{
+    
+    
+        public class A
+        {
+            public string Display()
+            {
+                return ("Hello");
+            }
+            public string Print()
+            {
+                return ("Good Morning");
+            }
+        }
+    
+}
---
+++ week-6/ExtensionMethod/ExtensionMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    public static class B
+    {
+        public static void NewMethod(this A ob)
+        {
+            Console.WriteLine("How are u!");
+        }
+
+    }
+    public  class Program
+    {
+       
+        static void Main(string[] args)
+        {
+                A ob = new A();
+               Console.WriteLine( ob.Display());
+            Console.WriteLine(ob.Print());
+            ob.NewMethod();
+              // Console.ReadKey();
+            Console.ReadLine();
+
+    }
+    }
+}
