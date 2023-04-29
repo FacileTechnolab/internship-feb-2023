99888faf5d5933a23e242928e60e5079cc7aa349
DESKTOP-F6DTSD6\admin <krishika.facile@outlook.com>
2023-04-28 17:03:51 -0700

    # c# intermediate.

---
+++ week-5/Delegates/Delegates/Photo.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Remoting.Messaging;
+
+namespace Delegates
+{
+    public class Photo
+    {
+        public static Photo Load(string path)
+    {
+        return new Photo();
+    }
+    public void Save() 
+    {
+
+    }
+} 
+}
---
+++ week-5/Lambda/Lambda/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, bool> val= (x, y) => x == y;
+
+
+            Console.WriteLine(val(5, 7));
+            Console.ReadLine();
+        }
+    }
+
+}
