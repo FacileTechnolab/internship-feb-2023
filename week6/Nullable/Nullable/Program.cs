ef1d659d35547d37b0732c5829e8f3c974d1e0f5
jahanvi <jahanvi.facile@outlook.com>
2023-04-29 18:04:01 +0530

    #Add files about Nullable and Dynamic.

---
+++ week6/Nullable/Nullable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullable
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<int> n = null;
+            Console.WriteLine(n.GetValueOrDefault());
+            int? n1 = 47;
+            Console.WriteLine(n1.GetValueOrDefault());
+            int? n3 = null;
+            Console.WriteLine(n3.GetValueOrDefault());
+            
+            dynamic name = "jahanvi";
+            name = 10;
+            Console.WriteLine(name);
+            Console.ReadLine();
+
+
+        }
+    }
+}
