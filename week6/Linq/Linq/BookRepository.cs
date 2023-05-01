929101923788c73a6e294ed3cb47c2ecf965f792
jahanvi <jahanvi.facile@outlook.com>
2023-05-01 17:49:05 +0530

    #add files about Linq , Asynchronous.

---
+++ week6/Linq/Linq/BookRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    internal class BookRepository
+    {
+        public IEnumerable<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book() {Title="Ado.net Step by step ",Price=5},
+                 new Book() {Title="Asp.net MVC ",Price=9.99f},
+                 new Book() {Title="Asp.net web Api",Price=12},
+                 new Book() {Title="C# Advanced Topoics",Price=7},
+                  new Book() {Title="C# Advanced Topoics",Price=9}
+
+
+
+        };
+            }
+    }
+
+    }
+
---
+++ week6/Linq/Linq/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var books=new BookRepository().GetBooks();
+            //var book = books.SingleOrDefault(b => b.Title == "Asp.net MVC");
+            //Console.WriteLine(book==null);
+            //var book = books.First(b => b.Title == "C# Advanced Topoics");
+            //Console.WriteLine(book.Title+""+book.Price);
+            //var count = books.Max(b => b.Price);
+            //var count1= books.Min (b=> b.Price);
+            //Console.WriteLine(count);
+            //Console.WriteLine(count1);
+
+
+
+            //Linq Extension Method
+            books.Single();
+            books.SingleOrDefault();
+            books.First();
+            books.FirstOrDefault();
+            books.Last();
+            books.LastOrDefault();
+            books.Min();
+            books.Count();
+            books.Average(b => b.Price);
+            books.Skip(5).Take(5);
+            var totalPrices=books.Sum(b=>b.Price);
+            Console.WriteLine(totalPrices);
+            Console.ReadLine();
+            //Linq Query Operator
+            //var cheapBooks =
+            //    from b in books
+            //    where b.Price < 10
+            //    orderby b.Title
+            //    select b.Title;
+            ////Linq Extension Methods
+            //var cheapBooks = books
+            //             .Where(b => b.Price < 10)
+            //             .OrderBy(b => b.Title)
+            //             .Select(b => b.Title); 
+            //var cheapBooks=new List<Book>();
+            //foreach (var book in books)
+            //{
+            //    if(book.Price < 10)
+            //        cheapBooks.Add(book);
+            //}
+            //foreach (var book in cheapBooks)
+            //    Console.WriteLine(book.Title+""+book.Price);
+        }
+    }
+}
