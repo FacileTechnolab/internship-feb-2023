d9d27927aa451b013c14e8b0ee6de01836565cea
DESKTOP-F6DTSD6\admin <krishika.facile@outlook.com>
2023-05-01 17:50:44 -0700

    #Dynamic in c# Advanced.

---
+++ week-6/Dynamic/Dynamic/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic a = 10;
+            dynamic b = 20;
+            var c = a + b ;
+
+        }
+    }
+}
