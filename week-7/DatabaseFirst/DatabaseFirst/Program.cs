00308b3ea92a054069e97ea99952c51a44209e67
DESKTOP-F6DTSD6\admin <krishika.facile@outlook.com>
2023-05-02 16:16:18 -0700

    #Databasefirst and Codefirst in EntityFramework.

---
+++ week-7/DatabaseFirst/DatabaseFirst/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new DemoEntities();
+            var post = new Post1()
+            {
+                Body = "Body",
+                DatePublishes = DateTime.Now,
+                Title = "Title",
+                PostId = 1
+            };
+            context.Post1.Add(post);
+            context.SaveChanges();
+        }
+    }
+}
