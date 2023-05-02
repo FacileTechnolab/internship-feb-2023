37fa4e45895ed5a59f6eff273686cde2cff503b1
jahanvi <jahanvi.facile@outlook.com>
2023-05-02 16:15:55 +0530

    #Add files about Database First , Code First.

---
+++ week7/DatabaseFirst/DatabaseFirst/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var context=new DEMOEntities();
+            var post = new Post()
+            {
+                Body = "Body",
+                DatePublished = DateTime.Now,
+                Title = "Title",
+                PostId = 1
+            };
+            context.Posts.Add(post);    
+            context.SaveChanges();
+        }
+    }
+}
