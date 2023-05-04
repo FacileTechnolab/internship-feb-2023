ec24a8283d51d5e43fcdb7dedd489c36cf95234d
shivangi <shivangi.facile@outlook.com>
2023-05-03 19:02:52 -0700

    #Code first and Edmx design pattern

--- week-7/section-2/EDMX_Designer/EDMX_Designer/Program.cs
+++ week-7/section-2/EDMX_Designer/EDMX_Designer/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,31 @@ using System.Threading.Tasks;
 
 namespace EDMX_Designer
 {
-    internal class Program
+
+    public enum Level:byte
+    {
+        Beginner=1,
+        intermediate=2,
+        Advance=3
+    }
+     class Program
     {
         static void Main(string[] args)
         {
+
+
+            //var course = new Cours();
+            //course.Level = Level.Beginner;
+
+            var dbContext = new skilaEntities2();
+
+            var courses = dbContext.GetCourses();
+
+            foreach (var a in courses)
+            {
+                Console.WriteLine(a.CourseID);
+                Console.ReadLine();
+            }
         }
     }
 }
