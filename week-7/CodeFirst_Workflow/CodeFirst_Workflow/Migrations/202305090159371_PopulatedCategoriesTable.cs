cd6600f108da3c954b971903b43a44db8b0b3a98
Dipali <dipali.facile@outlook.com>
2023-05-08 19:06:07 -0700

    Entity Framework : Seed Database

---
+++ week-7/CodeFirst_Workflow/CodeFirst_Workflow/Migrations/202305090159371_PopulatedCategoriesTable.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeFirst_Workflow.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulatedCategoriesTable : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
