0730549211e62afaf0474e339bb4613d1f1b6e60
DESKTOP-F6DTSD6\admin <krishika.facile@outlook.com>
2023-05-15 18:58:02 -0700

    #Adding Confugration, Create and injecting greeting, and Startup and middleware.

--- week8/OdeToCode/OdeToCode/Startup.cs
+++ week8/OdeToCode/OdeToCode/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,42 +26,56 @@ namespace OdeToCode
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, 
-                                IWebHostEnvironment env,
-                                IConfiguration  configuration) 
+        public void Configure(IApplicationBuilder app,
+                                IHostEnvironment env,
+                                IGreeter greeter, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            //app.UseDefaultFiles();
+            //app.UseStaticFiles();
+            app.UseFileServer();
             app.Run(async (context) =>
             {
-                var greeting = configuration["Greeting"];
-                await context.Response.WriteAsync("Hello World!");
-            });
+                var greeting = greeter.GetMessageOfTheDay();
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                await context.Response.WriteAsync($"{greeting}: {env.EnvironmentName}");
             });
         }
+
+        //else
+        //{
+        //    app.UseExceptionHandler("/Home/Error");
+        //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        //    app.UseHsts();
+        //app.Use(next =>
+        //{
+        //    return async context =>
+        //    {
+        //        logger.LogInformation("Requst incoming");
+        //        if (context.Request.Path.StartsWithSegments("/mym"))
+        //        {
+        //            await context.Response.WriteAsync("Hit!!");
+        //        }
+        //        else
+        //        {
+        //            await next(context);
+        //            logger.LogInformation("Response outgoing");
+        //        }
+
+        //    };
+        //});
+        //app.UseWelcomePage(new WelcomePageOptions
+        //{
+        //    Path="/wp"
+        //});
+
     }
 }
