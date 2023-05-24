69ee5bf5ef9f27f54640c3b2ca4d33b16a09c6b8
jahanvi <jahanvi.facile@outlook.com>
2023-05-24 18:47:01 +0530

    # Add files setUp Angular and Typescript.

---
+++ week-9/ts-hello/main.js
@@ -0,0 +1,18 @@
+// function log(message) {
+//     console.log(message);
+// }
+// var message = "hello world";
+// log(message);
+
+var number=1;
+let count=2;
+
+function doSomething()
+{
+    for(var i=0;i<5;i++)
+    {
+        console.log(i);
+    }
+    console.log('finally');
+}
+
