244749b4e7de965813f8c6f08f75a169ad65484f
jahanvi <jahanvi.facile@outlook.com>
2023-05-25 14:49:12 +0530

    # Learn about typescript.

--- week-9/ts-hello/main.js
+++ week-9/ts-hello/main.js
@@ -1,18 +1,37 @@
-// function log(message) {
+// function log(message)
+// {
 //     console.log(message);
 // }
-// var message = "hello world";
+// var message="hello world";
 // log(message);
-
-var number=1;
-let count=2;
-
-function doSomething()
-{
-    for(var i=0;i<5;i++)
-    {
-        console.log(i);
-    }
-    console.log('finally');
-}
-
+// function doSomething()
+// {
+//     for( let i=0;i<5;i++)
+//     {
+//         console.log(i)
+//     }
+//     console.log('finally'+i);
+// }
+// doSomething();
+// let count1 = 5;
+// count1 = 'j';
+var c1 = 2;
+c1 = 'h';
+var a;
+var b;
+var c;
+var d;
+var e = [1, 2, 3];
+var f = [1, true, 'a,false'];
+var Colorred = 0;
+var Colorgreen = 1;
+var Colorblue = 2;
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["green"] = 1] = "green";
+    Color[Color["blue"] = 2] = "blue";
+    Color[Color["purple"] = 3] = "purple";
+})(Color || (Color = {}));
+;
+var backgroundColor = Color.Red;
--- week-9/ts-hello/main.ts
+++ week-9/ts-hello/main.ts
@@ -1,7 +1,37 @@
-function log(message)
-{
-    console.log(message);
+// function log(message)
+// {
+//     console.log(message);
 
-}
-var message="hello world";
-log(message);
+// }
+// var message="hello world";
+// log(message);
+
+// function doSomething()
+// {
+//     for( let i=0;i<5;i++)
+//     {
+//         console.log(i)
+//     }
+//     console.log('finally'+i);
+// }
+// doSomething();
+
+// let count1 = 5;
+// count1 = 'j';
+
+var c1=2;
+c1='h';
+
+let a:number;
+let b:boolean;
+let c:string;
+let d:any;
+let e:number[]=[1,2,3];
+let f:any[]=[1,true,'a,false'];
+
+const Colorred=0;
+const Colorgreen=1;
+const Colorblue=2;
+
+enum Color{Red=0,green=1,blue=2,purple=3};
+let backgroundColor=Color.Red;
