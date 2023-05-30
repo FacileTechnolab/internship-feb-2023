a64a668907c5cfadc6d019543e1f8ab89b0ce414
Dipali <dipali.facile@outlook.com>
2023-05-29 18:20:55 -0700

    Angular : variable, type assertion, classes, interfaces, arrow functions

--- week-9/main1.js
+++ week-9/main1.js
@@ -1,2 +1,39 @@
-var a = 6;
-console.log(a);
+function log(message) {
+    console.log(message);
+}
+var message = 'Hello World';
+log(message);
+function dosomthing() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log("finally:" + i);
+}
+dosomthing();
+//types variable
+var a;
+var b;
+var c;
+var d;
+var e = [1, 2, 3];
+var f = [1, true, 'a', false];
+var ColoreGreen = 0;
+var ColoredBlue = 1;
+var ColoredRed = 2;
+var Color;
+(function (Color) {
+    Color[Color["Green"] = 0] = "Green";
+    Color[Color["Blue"] = 1] = "Blue";
+    Color[Color["Red"] = 2] = "Red";
+})(Color || (Color = {}));
+;
+var backgroundColor = Color.Blue;
+console.log(backgroundColor);
+//type assertion
+var str = "geeksforgeeks";
+console.log(str);
+var len = str.length;
+console.log(len);
+//Arrow functio
+var sum = function (a, b) { return a + b; };
+console.log("SUM: " + sum(5, 15));
--- week-9/main1.ts
+++ week-9/main1.ts
@@ -1,2 +1,45 @@
-var a=6;
-console.log(a);
+function log(message) {
+     console.log(message);
+ }
+
+var message = 'Hello World';
+log(message);
+
+
+
+function dosomthing(){
+     for(var i = 0; i < 5 ; i++)
+    {
+        console.log(i);
+    }
+    console.log("finally:" + i);
+}
+ dosomthing();
+
+//types variable
+let a : number;
+let b : boolean;
+let c : string;
+let d : any;
+let e : number[] = [1, 2, 3];
+let f : any[] = [1, true, 'a', false];
+
+
+const  ColoreGreen = 0;
+const  ColoredBlue = 1;
+const  ColoredRed = 2;
+
+enum  Color {Green = 0, Blue = 1, Red= 2 };
+let backgroundColor = Color.Blue;
+console.log(backgroundColor);
+
+//type assertion
+let str: unknown = "geeksforgeeks";
+console.log(str);
+  
+let len: number = (str as string).length;
+console.log(len);
+
+//Arrow functio
+let sum = (a: number, b: number) => a + b;  
+console.log("SUM: " +sum(5, 15));  
