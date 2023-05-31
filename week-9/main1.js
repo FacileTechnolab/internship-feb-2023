b0b1721bb2173b5ac01e2e4c4dd494163448f0bd
Dipali <dipali.facile@outlook.com>
2023-05-30 18:21:12 -0700

    Angular: interfaces, constructor, properties,access modifiers

--- week-9/main1.js
+++ week-9/main1.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 function log(message) {
     console.log(message);
 }
@@ -37,3 +52,89 @@ console.log(len);
 //Arrow functio
 var sum = function (a, b) { return a + b; };
 console.log("SUM: " + sum(5, 15));
+//class
+// class Point {
+//     x: number;
+//     y: number;
+//     draw(){
+//         console.log('X: '+this.x+ ', Y: '+this.y);
+//     }
+//     getDistance(another: Point){
+//     }
+// }
+// let point : Point=new Point;
+// point.x=89;
+// point.y=76;
+// point.draw();
+//counstuctor
+var Counst = /** @class */ (function () {
+    function Counst(a, b) {
+        a = a;
+        b = b;
+    }
+    Counst.prototype.add = function () {
+        console.log('A: ' + this.a + ', B: ' + this.b);
+    };
+    return Counst;
+}());
+var counst = new Counst();
+counst.add();
+//access modifier
+//public
+var Student = /** @class */ (function () {
+    function Student() {
+    }
+    return Student;
+}());
+var stud = new Student();
+stud.stuCode = 1022;
+stud.stuName = "Mosh Gameti";
+console.log(stud.stuCode + " " + stud.stuName);
+//private
+var StudentPriva = /** @class */ (function () {
+    function StudentPriva(code, name) {
+        this.studpriCode = code;
+        this.studpriName = name;
+    }
+    StudentPriva.prototype.display = function () {
+        return ("My uniquecode:".concat(this.studpriCode, ", my name : ").concat(this.studpriName, "."));
+    };
+    return StudentPriva;
+}());
+var stuprivate = new StudentPriva(1234, "fhdsfhkdsfd");
+console.log(stuprivate.display());
+//protected
+var StudeProt = /** @class */ (function () {
+    function StudeProt(codepro, namepro) {
+        this.studproCode = codepro;
+        this.studproName = namepro;
+    }
+    return StudeProt;
+}());
+var Person = /** @class */ (function (_super) {
+    __extends(Person, _super);
+    function Person(codepro, namepro, depart) {
+        var _this = _super.call(this, codepro, namepro) || this;
+        _this.depart = depart;
+        return _this;
+    }
+    Person.prototype.getElevatorPitch = function () {
+        return ("My unique code: ".concat(this.studproCode, ", my name: ").concat(this.studproName, " and I am in ").concat(this.depart, " Branch."));
+    };
+    return Person;
+}(StudeProt));
+var joeRoot = new Person(1, "JoeRoot", "CS");
+console.log(joeRoot.getElevatorPitch());
+//readonly
+var Company = /** @class */ (function () {
+    function Company(countName) {
+        this.country = "India";
+        this.name = countName;
+    }
+    Company.prototype.showDetail = function () {
+        console.log(this.name + " " + this.country);
+    };
+    return Company;
+}());
+var comp = new Company("JovaPoint");
+comp.showDetail();
--- week-9/main1.ts
+++ week-9/main1.ts
@@ -44,4 +44,103 @@ console.log(len);
 let sum = (a: number, b: number) => a + b;  
 console.log("SUM: " +sum(5, 15));  
 
+//class
+// class Point {
+//     x: number;
+//     y: number;
 
+//     draw(){
+//         console.log('X: '+this.x+ ', Y: '+this.y);
+//     }
+//     getDistance(another: Point){
+
+//     }
+// }
+// let point : Point=new Point;
+// point.x=89;
+// point.y=76;
+// point.draw();
+
+//counstuctor
+class Counst{
+    a: number;
+    b: number;
+
+
+    constructor(a?: number, b?: number){
+         a = a;
+         b = b;
+    }
+    add(){
+        console.log('A: '+this.a+ ', B: '+this.b);
+    }
+}
+let  counst : Counst= new Counst();
+counst.add();
+
+
+//access modifier
+  //public
+  class Student{
+    public stuCode: number;
+    stuName: string;
+  }
+  let stud = new Student();
+  stud.stuCode = 1022;
+  stud.stuName = "Mosh Gameti";
+
+  console.log(stud.stuCode + " "+ stud.stuName);
+
+  //private
+  class StudentPriva{
+    public studpriCode: number;
+    private studpriName: string;
+    constructor(code: number, name: string){
+     this.studpriCode = code;
+     this.studpriName = name;
+     
+    }
+    public display(){
+        return (`My uniquecode:${this.studpriCode}, my name : ${this.studpriName}.`);
+    }
+  }
+  let stuprivate:StudentPriva = new StudentPriva(1234, "fhdsfhkdsfd");
+  console.log(stuprivate.display());
+
+  //protected
+  class StudeProt{
+    public studproCode : number;
+    protected studproName : string;
+    constructor(codepro:number, namepro:string){
+         this.studproCode = codepro;
+         this.studproName = namepro;
+
+    }
+   
+  }
+  class Person extends StudeProt{
+    private depart:string;
+    constructor(codepro:number, namepro:string, depart:string){
+        super(codepro, namepro);
+        this.depart = depart;
+    }
+    public getElevatorPitch() {  
+        return (`My unique code: ${this.studproCode}, my name: ${this.studproName} and I am in ${this.depart} Branch.`);  
+    }
+  }
+  let joeRoot: Person = new Person(1, "JoeRoot", "CS");  
+  console.log(joeRoot.getElevatorPitch());  
+
+  //readonly
+  class Company{
+    readonly country:string = "India";
+    readonly name: string;
+    constructor(countName:string){
+        this.name = countName;
+    }
+    showDetail(){
+        console.log(this.name+ " "+ this.country);
+    }
+  }
+  let comp:Company = new Company("JovaPoint");
+  comp.showDetail();
