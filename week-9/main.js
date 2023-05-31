a064f9dfb1a24d1f315954055b978cb4af232cfb
DESKTOP-F6DTSD6\admin <krishika.facile@outlook.com>
2023-05-30 19:17:58 -0700

    #class,interface, object, constructor,access modifer.

--- week-9/main.js
+++ week-9/main.js
@@ -13,21 +13,32 @@
 //     console.log('Finally:' );
 // }
 // dosomething();
-var a;
-var b;
-var c;
-var d;
-var e = [1, 2, 3];
-var f = [1, true, 'a', false];
-var ColorRed = 0;
-var ColorGreen = 1;
-var ColorBlue = 2;
-var Color;
-(function (Color) {
-    Color[Color["Red"] = 0] = "Red";
-    Color[Color["Green"] = 1] = "Green";
-    Color[Color["Blue"] = 2] = "Blue";
-    Color[Color["Purple"] = 3] = "Purple";
-})(Color || (Color = {}));
-;
-var backgroundColor = Color.Red;
+// let a: number;
+// let b: boolean;
+// let c: string;
+// let d: any;
+// let e: number[] = [1,2,3];
+// let f: any[] =[1, true, 'a', false];
+// const ColorRed = 0;
+// const ColorGreen = 1;
+// const ColorBlue = 2;
+// enum Color{ Red=0, Green=1, Blue=2, Purple=3};
+// let backgroundColor = Color.Red;
+// let message;
+// message = 'abc';
+// let endsWithC = (<string>message).endsWith('c');
+// let alternativeWay = (message as string).endsWith('c');
+// let sum = (x: number, y:number)  =>   x+y;
+// console.log("Sum:" +sum (10,20));
+var Point = /** @class */ (function () {
+    function Point(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    Point.prototype.draw = function () {
+        console.log('X: ' + this.x + ' Y: ' + this.y);
+    };
+    return Point;
+}());
+var point = new Point(1, 2);
+point.draw();
--- week-9/main.ts
+++ week-9/main.ts
@@ -16,29 +16,45 @@
 // }
 // dosomething();
 
-let a: number;
-let b: boolean;
-let c: string;
-let d: any;
-let e: number[] = [1,2,3];
-let f: any[] =[1, true, 'a', false];
+// let a: number;
+// let b: boolean;
+// let c: string;
+// let d: any;
+// let e: number[] = [1,2,3];
+// let f: any[] =[1, true, 'a', false];
  
-const ColorRed = 0;
-const ColorGreen = 1;
-const ColorBlue = 2;
+// const ColorRed = 0;
+// const ColorGreen = 1;
+// const ColorBlue = 2;
 
-enum Color{ Red=0, Green=1, Blue=2, Purple=3};
-let backgroundColor = Color.Red;
+// enum Color{ Red=0, Green=1, Blue=2, Purple=3};
+// let backgroundColor = Color.Red;
 
 
-let message;
-message = 'abc';
-let endsWithC = (<string>message).endsWith('c');
-let alternativeWay = (message as string).endsWith('c');
+// let message;
+// message = 'abc';
+// let endsWithC = (<string>message).endsWith('c');
+// let alternativeWay = (message as string).endsWith('c');
 
-let log = function(message)
-{
-    console.log(message);
+// let sum = (x: number, y:number)  =>   x+y;
+// console.log("Sum:" +sum (10,20));
+
+class Point{
+    private x: number;
+    y: number;
+
+    constructor (x: number, y: number)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    draw()
+    {
+        console.log('X: ' + this.x + ' Y: ' + this.y); 
+    }
+// getDistance(another: Point){
+//     //..
+//  }
 }
-let doLog = () => console.log();
-
+let point = new Point(1,2);
+point.draw();
