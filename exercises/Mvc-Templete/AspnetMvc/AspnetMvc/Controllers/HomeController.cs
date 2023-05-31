e0731acf7103e9e54710f6e8a4a310b5e058e054
shivangi <shivangi.facile@outlook.com>
2023-05-31 12:37:00 -0700

    #Add Mvc templete

---
+++ exercises/Mvc-Templete/AspnetMvc/AspnetMvc/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspnetMvc.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		//public ActionResult About()
+		//{
+		//	ViewBag.Message = "Your application description page.";
+
+		//	return View();
+		//}
+
+		//public ActionResult Contact()
+		//{
+		//	ViewBag.Message = "Your contact page.";
+
+		//	return View();
+		//}
+
+        public ActionResult Home()
+        {
+            
+
+            return View();
+        }
+
+		public ActionResult Media()
+		{
+
+
+			return View();
+		}
+
+		public ActionResult Tickets()
+		{
+
+
+			return View();
+		}
+
+		public ActionResult Cities()
+		{
+
+
+			return View();
+		}
+
+		public ActionResult FAQ()
+		{
+
+
+			return View();
+		}
+
+		public ActionResult Comments()
+		{
+
+
+			return View();
+		}
+
+		public ActionResult Sponsorship()
+		{
+
+
+			return View();
+		}
+	}
+}
