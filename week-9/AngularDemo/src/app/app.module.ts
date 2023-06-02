a3057661d1b245e3d1fb1f057f491a40c0876161
Dipali <dipali.facile@outlook.com>
2023-06-01 19:18:09 -0700

    Angular : Components, Templates

--- week-9/AngularDemo/src/app/app.module.ts
+++ week-9/AngularDemo/src/app/app.module.ts
@@ -5,10 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent
+
+
   ],
   imports: [
     BrowserModule,
--- week-9/AngularDemo/src/app/courses/courses.component.ts
+++ week-9/AngularDemo/src/app/courses/courses.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-courses',
-  templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css']
-})
-export class CoursesComponent {
+  template: '<h4>Courses</h4> <h5>{{title}}</h5>'
+   
 
+ 
+})
+export class CoursesComponent  {
+  title = "List of courses";
 }
