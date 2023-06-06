2967a9b0f063d2db45bd696e179b43c270a690ee
shivangi <shivangi.facile@outlook.com>
2023-06-05 19:25:53 -0700

    #add Angular project

---
+++ week-9/Angular/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // use a title property
+  title = 'Angular app';
+  //use a variable name
+  name="janvi";
+  //print the function value
+  get()
+  {
+ return "Krishika";
+  }
+  // print the object 
+  obj={
+    bookname:"c language fundamentals",
+    price:120,
+    Authorname:"adam smith"
+  }
+  // print the array  
+  //we can define the more functionality 
+  arr=['c','c++','java','python','ruby'];
+
+  // we can print the url 
+  SiteUrl=window.location.href;
+  // event work 
+
+  Myevent(evt:any){
+    console.warn(evt);
+  }
+  getvalue($event:any){
+    console.warn($event.target.value);
+  }
+}
