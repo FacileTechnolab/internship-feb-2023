3e0a31b6a6dcc2402a2d38d02f6f25ff6ff89cfe
jahanvi <jahanvi.facile@outlook.com>
2023-06-08 18:17:20 +0530

    #add files about To set Header,footer,style binding,Adding bootstrap,Add material UI and pass data parent to child.

--- week-9/basic/src/app/app.module.ts
+++ week-9/basic/src/app/app.module.ts
@@ -5,18 +5,36 @@ import{FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AutherComponent } from './auther/auther.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgFor } from '@angular/common';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import{MatButtonModule} from '@angular/material/button'
+import{MatSliderModule} from '@angular/material/slider';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { UserComponent } from './user/user.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AutherComponent
+    AutherComponent,
+    HeaderComponent,
+    FooterComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AuthorsModule,
-    FormsModule
+    FormsModule,
+    NgbModule,
+    NgFor, 
+    NgbAlertModule, BrowserAnimationsModule,
+    MatButtonModule,MatSliderModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
