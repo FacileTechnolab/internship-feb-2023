9601f3abc570cad45607024b240ed2403cae59aa
DESKTOP-F6DTSD6\admin <krishika.facile@outlook.com>
2023-06-09 18:59:30 -0700

    #add bootstrap, add angular material ui, pass data parent to child

--- week-9-new/hello-new/src/app/app.component.ts
+++ week-9-new/hello-new/src/app/app.component.ts
@@ -1,4 +1,47 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+
+interface Alert {
+	type: string;
+	message: string;
+}
+
+const ALERTS: Alert[] = [
+	{
+		type: 'success',
+		message: 'This is an success alert',
+	},
+	{
+		type: 'info',
+		message: 'This is an info alert',
+	},
+	{
+		type: 'warning',
+		message: 'This is a warning alert',
+	},
+	{
+		type: 'danger',
+		message: 'This is a danger alert',
+	},
+	// {
+	// 	type: 'primary',
+	// 	message: 'This is a primary alert',
+	// },
+	// {
+	// 	type: 'secondary',
+	// 	message: 'This is a secondary alert',
+	// },
+	// {
+	// 	type: 'light',
+	// 	message: 'This is a light alert',
+	// },
+	// {
+	// 	type: 'dark',
+	// 	message: 'This is a dark alert',
+	// },
+];
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +50,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hello-new';
+  alerts: Alert[];
+
+	constructor() {
+		this.reset();
+	}
+
+	close(alert: Alert) {
+		this.alerts.splice(this.alerts.indexOf(alert), 1);
+	}
+
+	reset() {
+		this.alerts = Array.from(ALERTS);
+	}
+
+user = [{
+	name:"Mercy",
+	age:16
+},
+{
+	name:"Merry",
+	age:17
+},
+{
+	name:"jelly",
+	age:19
+},
+{
+	name:"julee",
+	age:42
 }
+]
+}
+
--- week-9-new/hello-new/src/app/app.module.ts
+++ week-9-new/hello-new/src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule} from '@angular/material/button';
+import {MatSliderModule} from '@angular/material/slider';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
