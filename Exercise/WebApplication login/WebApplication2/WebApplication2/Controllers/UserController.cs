3cb08e84f0e09f40ab516f6050f6e845d21389b7
jahanvi <jahanvi.facile@outlook.com>
2023-06-16 14:28:06 +0530

    #Add files about SignUp and SignIn.

---
+++ Exercise/WebApplication login/WebApplication2/WebApplication2/Controllers/UserController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class UserController : Controller
+    {
+        AppDb db = new AppDb();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Signin()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+      
+        public ActionResult Signin(Users users)
+
+        {
+            var obj=db.Users.Where(x=>x.Email == users.Email && x.Password == users.Password).FirstOrDefault();
+            if(users!=null)
+            {
+                Session["Email"] = obj.Email;
+                Session["User"] = obj.User;
+                TempData["LoginSuccessMessage"] = "<script>alert('Login Successful')";
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "<script>alert('Login Incorrect')";
+            }
+            return View();
+        }
+        //public ActionResult Signup()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
+        [HttpPost]
+        public ActionResult Signup(Users users)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                db.Users.Add(users);
+                db.SaveChanges();
+            }
+
+            return View();
+        }
+        public ActionResult SignOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+
+}
+
+
+
+
---
+++ Exercise/WebApplication login/WebApplication2/WebApplication2/Models/Users.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Users
+    {
+        [Key]
+       
+        public int Id { get;set; }
+
+        [DataType(DataType.Text)]
+       
+
+        [Required(ErrorMessage = "First name is Required"), MaxLength(30)]
+      
+
+        public string FirstName { get;set; }
+
+        [Required(ErrorMessage = "Last name is Required")]
+        public string LastName { get;set; }
+
+        [Required(ErrorMessage = "User name is Required")]
+        public string User { get;set; }
+
+        [Required(ErrorMessage = "Phone is Required")]
+        //[Range(0, 10, ErrorMessage = "Please enter the correct value")]
+
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Email is Required")]
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Your Email is not valid.")]
+        public string Email { get;set; }
+
+        [Required(ErrorMessage = "Password is Required")]
+        [DataType(DataType.Password)]
+        public string Password { get;set; }
+
+        [Required(ErrorMessage = "Confirm Password is Required")]
+        [DataType(DataType.Password)]
+
+        public string ConfirmPassword { get;set; }
+
+        
+    }
+}
