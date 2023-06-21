5fa7390ab723ed96f16cae3dad2b9e8855973573
Dipali <dipali.facile@outlook.com>
2023-06-21 11:45:46 -0700

    Exercises : Html to angular convert cretae project

--- Exercises/AngularProject/src/app/app-routing.module.ts
+++ Exercises/AngularProject/src/app/app-routing.module.ts
@@ -1,28 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { PartnersComponent } from './partners/partners.component';
-import { MarketingComponent } from './marketing/marketing.component';
-import { FaqComponent } from './faq/faq.component';
 
-const routes: Routes = [
-  {
-    path:'Home',
-    component:HomeComponent
-  },
-  {
-    path:'Partners',
-    component:PartnersComponent
-  },
-  {
-    path:'Marketing',
-    component:MarketingComponent
-  },
-  {
-    path:'Faq',
-    component:FaqComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Exercises/AngularProject/src/app/app.component.spec.ts
+++ Exercises/AngularProject/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularProject'`, () => {
+  it(`should have as title 'Angularproject'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularProject');
+    expect(app.title).toEqual('Angularproject');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('AngularProject app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Angularproject app is running!');
   });
 });
