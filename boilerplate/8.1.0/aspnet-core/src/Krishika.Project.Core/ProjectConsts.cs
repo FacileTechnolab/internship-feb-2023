186a2b382ecb3b001f5911c41f65f0f566a6f03a
DESKTOP-F6DTSD6\admin <krishika.facile@outlook.com>
2023-06-26 19:04:59 -0700

    # Asp .net boilerplate run the project.

---
+++ boilerplate/8.1.0/aspnet-core/src/Krishika.Project.Core/ProjectConsts.cs
@@ -0,0 +1,20 @@
+﻿using Krishika.Project.Debugging;
+
+namespace Krishika.Project
+{
+    public class ProjectConsts
+    {
+        public const string LocalizationSourceName = "Project";
+
+        public const string ConnectionStringName = "Default";
+
+        public const bool MultiTenancyEnabled = true;
+
+
+        /// <summary>
+        /// Default pass phrase for SimpleStringCipher decrypt/encrypt operations
+        /// </summary>
+        public static readonly string DefaultPassPhrase =
+            DebugHelper.IsDebug ? "gsKxGZ012HLL3MI5" : "bd95288e7eae4fd6a0f737b0eb22c83a";
+    }
+}
---
+++ boilerplate/8.1.0/aspnet-core/src/Krishika.Project.EntityFrameworkCore/EntityFrameworkCore/ProjectDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Abp.Zero.EntityFrameworkCore;
+using Krishika.Project.Authorization.Roles;
+using Krishika.Project.Authorization.Users;
+using Krishika.Project.MultiTenancy;
+
+namespace Krishika.Project.EntityFrameworkCore
+{
+    public class ProjectDbContext : AbpZeroDbContext<Tenant, Role, User, ProjectDbContext>
+    {
+        /* Define a DbSet for each entity of the application */
+        
+        public ProjectDbContext(DbContextOptions<ProjectDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
